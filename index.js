528d295286c73b9fcf7fffe37d48c4b0beaf7e1d
Mohiuddin Sikder <mohi.info20@gmail.com>
2021-10-29 18:50:36 +0600

    added booking

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 async function run(){
    try{
       // Connect the client to the server
