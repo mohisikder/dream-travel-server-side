e460e4048deb80982efc41948513ce6356205160
Mohiuddin Sikder <mohi.info20@gmail.com>
2021-10-29 21:31:50 +0600

    database added

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
+const port = process.env.PORT || 5000
 
 // Middleware
 app.use(cors())
+app.use(express.json());
 
-const port = process.env.PORT || 5000
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zz9qt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("dream_travel_db");
+    const toursCollection = database.collection("tours");
+    console.log('database connected successfully');
+    
+   
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res)=>{
    res.send('Hello World!!')
