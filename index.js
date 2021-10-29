12b3b2e91ad4ae55a8f911bdd272240949e39d35
Mohiuddin Sikder <mohi.info20@gmail.com>
2021-10-29 19:49:38 +0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
 const app = express()
+
+// Middleware
+app.use(cors())
+
 const port = process.env.port || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res)=>{
+   res.send('Hello World!!')
 })
 
-app.listen(port, () => {
-  console.log(`dream travel server listening at http://localhost:${port}`)
+app.get('/products/:id', (req, res)=> {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(port, () =>{
+  console.log('Running web server listening on port', port)
 })
